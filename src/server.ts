b70d64a65d5853114637ea1312d33d0971d111c1
elvinciqueira <elvinciqueira@gmail.com>
2020-10-15 17:44:06 -0300

    abstraindo rotas

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,12 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
 
 import './database/connection';
 
-import User from './models/Users';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/users', async (request, response) => {
-  const { email, password } = request.body;
-
-  const usersRepository = getRepository(User);
-
-  const user = await usersRepository.create({ email, password });
-
-  await usersRepository.save(user);
-
-  return response.status(200).json(user);
-})
+app.use(routes);
 
 app.listen(3000, () => console.log('server is running on port 3000'));
