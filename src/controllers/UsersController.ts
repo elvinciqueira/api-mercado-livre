518b6458ed656418cacd8096bb301a7ef46369bd
elvinciqueira <elvinciqueira@gmail.com>
2020-10-15 18:00:50 -0300

    abstraindo rota para controller

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import User from '../models/Users';
+
+export default {
+  async create(request: Request, response: Response) {
+    const { email, password } = request.body;
+
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.create({ email, password });
+
+    await usersRepository.save(user);
+
+    return response.status(200).json(user);
+  }
+}
--- src/routes/usersRouter.router.ts
+++ src/routes/usersRouter.router.ts
@@ -1,20 +1,9 @@
-import { Router, Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { Router } from 'express';
 
-import User from '../models/Users';
+import UsersController from '../controllers/UsersController';
 
 const usersRouter = Router();
 
-usersRouter.post('/', async (request: Request, response: Response) => {
-  const { email, password } = request.body;
-
-  const usersRepository = getRepository(User);
-
-  const user = await usersRepository.create({ email, password });
-
-  await usersRepository.save(user);
-
-  return response.status(200).json(user);
-});
+usersRouter.post('/', UsersController.create);
 
 export default usersRouter;
