f97ea64f7ee29ba35e61cf094224d84b6f558852
elvinciqueira <elvinciqueira@gmail.com>
2020-10-15 19:06:45 -0300

    fix

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import UserController from '../controllers/UsersController';
 
 import UsersController from '../controllers/UsersController';
 
+const usersController = new UsersController();
+
 const usersRouter = Router();
 
-usersRouter.post('/', UsersController.create);
+usersRouter.post('/', usersController.create);
 
 export default usersRouter;
